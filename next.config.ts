aeb8767e7a9aab0a886321aedf99883464500a72
Scottiieee <splendidhoodies@gmail.com>
2025-12-18 02:22:52 +0800

    feat: update next.config.ts to include productionBrowserSourceMaps and typescript settings

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  productionBrowserSourceMaps: false,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
